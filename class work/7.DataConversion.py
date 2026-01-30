39a016450a194db2215523d2e583fd8fad7349e7
AjayReddy Pammi <pammiajayreddy2002@gmail.com>
2026-01-30 11:18:07 +0530

    Data Conversion

---
+++ class work/7.DataConversion.py
@@ -0,0 +1,39 @@
+'''
+data conversion
+'''
+a=10 #Converting from int
+print(float(a))
+print(str(a))
+print(complex(a))
+print(bool(a))
+b=12.3 #Converting from float
+print(int(b))
+print(str(b))
+print(complex(b))
+print(bool(b))
+s="python " #Converting from str
+print(bool(s))
+print(list(s))
+print(tuple(s))
+print(set(s))
+l=[1,2,3,4] #Converting from list
+print(str(l))
+print(bool(l))
+print(tuple(l))
+print(set(l))
+p=(1,2,3,4,5,6) #Converting from tuple
+print(bool(p))
+print(str(p))
+print(list(p))
+print(set(p))
+s1={1,2,3,4,5,6,7,8} #Converting from set
+print(bool(s1))
+print(str(s1))
+print(list(s1))
+print(tuple(s1))
+d={1:2,4:5,6:7} #Converting from dict
+print(bool(d))
+print(str(d))
+print(list(d))
+print(tuple(d))
+print(set(d))
